6f91cb0b2eedd74edefa6ddb962857d4ec733182
maocat <zk246422@163.com>
2018-10-26 17:45:00 +0800

    backend

--- app/blog/view/backend.py
+++ app/blog/view/backend.py
@@ -35,7 +35,26 @@ def backend_home():
 @bp.route("/detail", methods=["GET"])
 @login_required
 def backend_detail():
-    return tmpl(admin=current_user)
+    key = request.args.get("key", "")
+    tag = request.args.get("tag", "")
+    create_time = request.args.get("order", "-create_time")
+    print(create_time)
+    query = {}
+    if key:
+        titles = ArticleBlog.objects(title__icontains=key).all()
+        bodys = ArticleBlog.objects(body__icontains=str(key)).all()
+        query['id__in'] = [t.id for t in titles] + [b.id for b in bodys]
+    if tag:
+        query["tag"] = tag
+    articles = ArticleBlog.objects(**query).order_by(create_time).all()
+    if create_time =="create_time":
+        create_time ="-create_time"
+    else:
+        create_time = "create_time"
+    return tmpl(admin=current_user,articles=articles,create_time=create_time)
+
+
+
 
 
 @bp.route("/logout", methods=["GET"])
@@ -67,6 +86,9 @@ def backend_tag_add():
         tag.save()
         return redirect(url_for("backend.backend_tag_list"))
     return tmpl("utils/bash_form.html",form=form, admin=current_user)
+
+
+
 
 @bp.route("/tag/list", methods=["GET", "POST"])
 @login_required
@@ -107,15 +129,60 @@ def backend_tag_ajax():
     return jsonify(a)
 
 
-
 @bp.route("/add/article", methods=["GET", "POST"])
 @login_required
 def backend_article_add():
     if request.method == "GET":
         return tmpl(admin=current_user)
     else:
-        print(request.form.getlist("tags[]"))
-        return tmpl(admin=current_user)
+        article = ArticleBlog()
+        article.title = request.form['title']
+        article.body = request.form['text']
+        tag_list = []
+        for tag in request.form.getlist("tags[]"):
+            tag_list.append(TagBlog.objects(id=tag).first())
+        article.tags = tag_list
+        article.save()
+
+        return jsonify(url_for("backend.backend_detail"))
+
+
+@bp.route("/edit/article/<id>", methods=["GET", "POST"])
+@login_required
+def backend_article_edit(id):
+    article = ArticleBlog.objects(id=id).first()
+    if request.method == "GET":
+        tags = TagBlog.objects.all()
+        return tmpl(admin=current_user,article=article,tags=tags)
+    else:
+        article.title = request.form['title']
+        article.body = request.form['text']
+        tag_list = []
+        for tag in request.form.getlist("tags[]"):
+            tag_list.append(TagBlog.objects(id=tag).first())
+        article.tags = tag_list
+        article.save()
+
+        return jsonify(url_for("backend.backend_detail"))
+
+
+@bp.route("/article/<id>/status", methods=["GET"])
+@login_required
+def backend_article_status(id):
+    article = ArticleBlog.objects(id=id).first()
+    if article.status == ArticleBlog.STATUS.off:
+        article.status = ArticleBlog.STATUS.on
+    else:
+        article.status = ArticleBlog.STATUS.off
+    article.save()
+    return redirect(request.referrer)
+
+
+@bp.route("/article/<id>/detail", methods=["GET"])
+@login_required
+def backend_article_detail(id):
+    article = ArticleBlog.objects(id=id).first()
+    return tmpl(article=article,admin=current_user)
 
 
 
