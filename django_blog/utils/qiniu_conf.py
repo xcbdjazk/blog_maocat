b22a7080425bb9334a6173a1bb6f305b9fc0b3cd
maocat <zk246422@163.com>
2019-05-09 16:23:13 +0800

    update .gitignore

---
+++ django_blog/utils/qiniu_conf.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+
+# UEDITOR_UPLOAD_PATH = ""
+
+
+class Config(object):
+    # UEDITOR_UPLOAD_TO_QINIU = settings.DEBUG
+    UEDITOR_UPLOAD_TO_QINIU = not settings.DEBUG
+    UEDITOR_QINIU_ACCESS_KEY = ""
+    UEDITOR_QINIU_SECRET_KEY = ""
+
+    # {
+    #     'name':'BUCKET_NAME',
+    #     'url':'DOMAIN',
+    # }
+    # qshell doc:--- > https://github.com/qiniu/qshell/blob/master/docs/qupload.md
+    BUCKET_CONF = {
+        'static':{
+            'name':'static',
+            'key_prefix':'backend/',
+            'src_dir':'backend/static',
+            'url':'http://static.maocatooo.cn/',
+        },
+        'upload':{
+            'name':'blog',
+            'key_prefix': 'upload/',
+            # 'src_dir': 'upload/',
+            'url':'http://upload.maocatooo.cn/',
+        }
+    }
