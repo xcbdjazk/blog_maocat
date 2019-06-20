5d60d242ba4c3b3e6fb90f57266143e9a2e6ec60
MAODOU <zk246422@163.com>
2019-06-20 22:56:14 +0800

    bugfix

--- django_blog/backend/helps/__init__.py
+++ django_blog/backend/helps/__init__.py
@@ -1,7 +1,7 @@
-
+from utils.qiniu_conf import Config
 def global_params(request):
     return {
-        'category_list': 1,
+        'qiniu_url': Config.BUCKET_CONF.get('static').get('url')+Config.BUCKET_CONF.get('static').get('key_prefix'),
         'ad_list': 2,
         'archive_list': 3
     }
