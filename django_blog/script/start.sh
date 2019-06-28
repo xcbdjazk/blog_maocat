633c238a1c94562e3e6c286cff70068c8039af50
maocat <zk246422@163.com>
2019-06-28 10:03:57 +0800

    bugfix 部署测试

--- django_blog/script/start.sh
+++ django_blog/script/start.sh
@@ -2,6 +2,6 @@
 cd /opt/webapps/django_blog
 pwd
 pip3 install -r requirements.txt
-unlink /var/run/supervisor/supervisor.sock
-unlink /tmp/supervisor.sock
-supervisord -c utils/supervisord.conf
+#unlink /var/run/supervisor/supervisor.sock
+#unlink /tmp/supervisor.sock
+supervisord -c /opt/webapps/django_blog/utils/supervisord.conf
