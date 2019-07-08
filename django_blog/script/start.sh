917a06afe32e700c82caec3e609bf3c5ad65d3b8
maocat <zk246422@163.com>
2019-07-08 11:32:11 +0800

    部署bugfix

--- django_blog/script/start.sh
+++ django_blog/script/start.sh
@@ -2,8 +2,8 @@
 cd /opt/webapps/django_blog
 pwd
 pip3 install -r requirements.txt
-python manage.py makemigrations backend
-python manage.py migrate
+python3 manage.py makemigrations backend
+python3 manage.py migrate
 #unlink /var/run/supervisor/supervisor.sock
 #unlink /tmp/supervisor.sock
 supervisord -c /opt/webapps/django_blog/utils/supervisord.conf
